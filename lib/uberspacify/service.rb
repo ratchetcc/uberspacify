02316f92b43651ac93d996e64a8cfa222dcb98d4
Michael Kuehl <mickuehl@googlemail.com>
2013-08-19 23:02:25 +0200

    added comments to service.rb

--- lib/uberspacify/service.rb
+++ lib/uberspacify/service.rb
@@ -1,14 +1,18 @@
 Capistrano::Configuration.instance.load do
   
+  desc "tasks to start/stop/restart the web server"
   namespace :service do
+    desc "start the web server"
     task :start do
       run "svc -u ~/service/#{fetch :daemon_service}"
     end
     
+    desc "stop the web server"
     task :stop do
       run "svc -d ~/service/#{fetch :daemon_service}"
     end
     
+    desc "restart the web server"
     task :restart do
       run "svc -du ~/service/#{fetch :daemon_service}"
     end
