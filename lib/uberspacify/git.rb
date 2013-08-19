f9187579002608bf790b86800764e6dd3ab97eab
Michael Kuehl <mickuehl@googlemail.com>
2013-08-20 00:52:55 +0200

    final version of git:pull. FTW...

--- lib/uberspacify/git.rb
+++ lib/uberspacify/git.rb
@@ -8,7 +8,11 @@ Capistrano::Configuration.instance.load do
       pull_cmd = "cd #{fetch :application_home} && git pull origin #{fetch :branch}"
       
       run pull_cmd do |channel, stream, out|
-        channel.send_data("#{fetch :scm_password}\n") if out =~ /Password:/
+        if out =~ /Password:/
+          channel.send_data("#{fetch :scm_password}\n")
+        else
+          puts out
+        end
       end
    
     end
