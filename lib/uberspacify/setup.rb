1b8ac5b375f6f6af7f05087fcf36ec29d9a2eee8
Michael Kuehl <mickuehl@googlemail.com>
2013-08-20 01:17:14 +0200

    added environment setup tasks

--- lib/uberspacify/setup.rb
+++ lib/uberspacify/setup.rb
@@ -4,7 +4,9 @@ Capistrano::Configuration.instance.load do
   namespace :setup do
     desc "Tasks to prepare a uberspace for deployment"
     task :default do
-      
+      ruby
+      apache
+      daemon_tools
     end
     
     desc "Setup ruby interpreter and tools"
@@ -14,6 +16,11 @@ Capistrano::Configuration.instance.load do
     
     desc "Setup daemon tools"
     task :daemon_tools do
+      
+    end
+    
+    desc "Setup Apache reverse-proxy & thin"
+    task :apache do
       
     end
     
