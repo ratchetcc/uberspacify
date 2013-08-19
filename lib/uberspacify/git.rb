205865344ef7c1e279cbb01012902eb574943ad9
Michael Kuehl <mickuehl@googlemail.com>
2013-08-19 23:46:10 +0200

    added git tasks

--- lib/uberspacify/git.rb
+++ lib/uberspacify/git.rb
@@ -1,12 +1,15 @@
 Capistrano::Configuration.instance.load do
-  
+
   desc "Git tasks"
   namespace :git do
-    
+
     desc "Pull the latest version from git"
     task :pull do
-      run "cd #{fetch :application_home} && git pull origin #{fetch :repo_branch}"
+      #run "cd #{fetch :application_home}" do |channel, stream, data|
+      #  channel.send_data("password\n")
+      #end
+      run "cd #{fetch :application_home} && git pull origin #{fetch :repo_branch}", options={:data => 'K0naBeach'}
     end
-    
+
   end
 end
