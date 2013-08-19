2dd959b96febc32b08f24282e1471fc37c8ace38
Michael Kuehl <mickuehl@googlemail.com>
2013-08-19 23:21:56 +0200

    added basic rake tasks

--- lib/uberspacify/rake.rb
+++ lib/uberspacify/rake.rb
@@ -5,31 +5,26 @@ Capistrano::Configuration.instance.load do
     
     desc "rake db:create"
     task :create do
-      run "cd #{fetch :application_home}"
-      run "bundle exec rake db:create RAILS_ENV=#{fetch :environment}"
+      run "cd #{fetch :application_home} && bundle exec rake db:create RAILS_ENV=#{fetch :environment}"
     end
     
     desc "rake db:setup"
     task :setup do
-      run "cd #{fetch :application_home}"
-      run "bundle exec rake db:setup RAILS_ENV=#{fetch :environment}"
+      run "cd #{fetch :application_home} && bundle exec rake db:setup RAILS_ENV=#{fetch :environment}"
     end
     
     desc "rake db:seed"
     task :seed do
-      run "cd #{fetch :application_home}"
-      run "bundle exec rake db:seed RAILS_ENV=#{fetch :environment}"
+      run "cd #{fetch :application_home} && bundle exec rake db:seed RAILS_ENV=#{fetch :environment}"
     end
     
     desc "rake db:migrate"
     task :migrate do
-      run "cd #{fetch :application_home}"
-      run "bundle exec rake db:migrate RAILS_ENV=#{fetch :environment}"
+      run "cd #{fetch :application_home} && bundle exec rake db:migrate RAILS_ENV=#{fetch :environment}"
     end
     
     desc "rake assets:precompile"
     task :precompile do
-      #run "cd #{fetch :application_home}"
       run "cd #{fetch :application_home} && bundle exec rake assets:precompile RAILS_ENV=#{fetch :environment}"
     end
     
